52a0d4530eaacb311694ac0608f62d5342d3f459
Aleks <aleksandar.bozov@outlook.com>
2026-04-21 09:20:54 +0100

    Vendor pytorch-two-GAN inference code (networks.py + license)

---
+++ src/court_detection/two_gan/__init__.py
@@ -0,0 +1,6 @@
+﻿"""Vendored two-GAN code from https://github.com/lood339/pytorch-two-GAN
+
+Only the inference-time components (UnetGenerator + define_G) are used.
+See NOTICE and LICENSE for attribution.
+"""
+from .networks import define_G
